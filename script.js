db003df733d9e6c091e1b44e4bd18df75837724c
liaojiedi <100394896+liaojiedi@users.noreply.github.com>
2022-04-17 00:09:10 +0800

    个人作业

---
+++ script.js
@@ -0,0 +1,25 @@
+
+function duxin(){
+var response1= prompt("首先，我先要问你几个问题，这很重要。你的性别是");
+var response2= prompt("你的星座是");
+var response3= prompt("你更喜欢猫还是狗？");
+alert("现在正式开始，接下来会出现八个词语，随便选择一个记在心中即可 ");
+alert("滑雪    鼻涕    垃圾箱   杯子   温泉    咖啡    年糕    冬天");
+alert("接下来，选择一个跟你刚刚选择的有关联的词语");
+alert(" 电话  计算机  馒头  雪  米  牛奶  餐巾纸  针");
+alert("现在，忘掉你第一次的选择，牢牢记住第二次的选择！从这八个形容词中选择与你第二次选择中最相符的描述！");
+alert(" 很窄  很多  很方  很暗  很陡  很甜   很白   很尖 ");
+alert("别急，这是最后一次了！");
+alert("从这八个词中，选择和你刚刚选的形容词，最贴切的那个并记在心中");
+alert(" 桌子  宇宙  铅笔  小刀  砂糖  狗屋  乌龟  金字塔");
+alert(" 正在读取你的内心······");}
+
+
+
+function showTopic(){
+  alert("   点击确定之后，会出现你刚刚所想的答案，别眨眼！");
+
+  var x = document.getElementById("demo");
+  x.style.fontsize="50px";
+  x.style.color="red";
+}
